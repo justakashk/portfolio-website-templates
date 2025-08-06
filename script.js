9bf3968771cfdd4a7910cf30f93fc53c43e24f43
Akash Kumar <150289222+justakashk@users.noreply.github.com>
2025-08-06 15:46:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling
+document.querySelectorAll('a.nav-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
